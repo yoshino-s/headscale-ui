72f88e21b6e6408f88f1fb70a4aa23cd64b09004
yoshino-s <cy-cui@outlook.com>
2024-02-23 17:47:22 +0800

    fix: vite path issue

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  base: '/web/',
   server: {
     proxy: {
       '/api/': 'https://headscale.yoshino-s.xyz',
